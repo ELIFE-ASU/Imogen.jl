c178cb413566428a1bd5f0a5eca150c501669d1d
Douglas G. Moore <doug@dglmoore.com>
2021-06-14 22:05:37 -0700

    :hammer: Support discrete multivariate active info with replicates

--- src/ai.jl
+++ src/ai.jl
@@ -1,52 +1,115 @@
-mutable struct ActiveInfo <: InfoDist
+mutable struct ActiveInfo{D} <: InfoDist
     joint::Matrix{Int}
     future::Vector{Int}
     history::Vector{Int}
-    b::Int
+    B::Int
+    bs::NTuple{D,Int}
     k::Int
     N::Int
-    function ActiveInfo(b::Int, k::Int)
-        if b < 2
+    function ActiveInfo(bs::NTuple{D, <:Integer}; k::Integer=1) where D
+        if any(b -> b < 2, bs)
             throw(ArgumentError("the support of the future must be at least 2"))
         elseif k < 1
             throw(ArgumentError("history length must be at least 1"))
         end
-        new(zeros(Int, b, b^k), zeros(Int, b), zeros(Int, b^k), b, k, 0)
+        B = prod(bs)
+        new{D}(zeros(Int, B, B^k), zeros(Int, B), zeros(Int, B^k), B, bs, k, 0)
     end
 end
 
-function ActiveInfo(xs::AbstractVector{Int}; k::Int=1)
+ActiveInfo(b::Integer; kwargs...) = ActiveInfo((b,); kwargs...)
+
+function ActiveInfo(xs::AbstractArray{Int,3}; k::Int=1)
     if k < 1
         throw(ArgumentError("history length must be at least 1"))
     elseif length(xs) ≤ k
         throw(ArgumentError("first argument's length must be greater than the history length"))
     end
-    xmin, xmax = extrema(xs)
-    if xmin < 1
-        throw(ArgumentError("observations must be positive, nonzero"))
+    xmax = max.(2, maximum(xs; dims=(2,3)))
+    observe!(ActiveInfo(tuple(xmax...); k), xs)
+end
+
+function ActiveInfo(xs::AbstractArray{Int,2}; k::Int=1)
+    if k < 1
+        throw(ArgumentError("history length must be at least 1"))
+    elseif length(xs) ≤ k
+        throw(ArgumentError("first argument's length must be greater than the history length"))
     end
-    observe!(ActiveInfo(max(2, xmax), k), xs)
+    xmax = max.(2, maximum(xs; dims=2))
+    observe!(ActiveInfo(tuple(xmax...); k), xs)
+end
+
+function ActiveInfo(xs::AbstractArray{Int,1}; k::Int=1)
+    if k < 1
+        throw(ArgumentError("history length must be at least 1"))
+    elseif length(xs) ≤ k
+        throw(ArgumentError("first argument's length must be greater than the history length"))
+    end
+    xmax = max(2, maximum(xs))
+    observe!(ActiveInfo(xmax; k), xs)
 end
 
 function estimate(dist::ActiveInfo)
     entropy(dist.future, dist.N) + entropy(dist.history, dist.N) - entropy(dist.joint, dist.N)
 end
 
-function observe!(dist::ActiveInfo, xs::AbstractVector{Int})
+function observe!(dist::ActiveInfo, xs::AbstractArray{Int,3})
+    if size(xs, 2) ≤ dist.k
+        throw(ArgumentError("data's length must be greater than the history length"))
+    end
+    dist.N += (size(xs, 2) - dist.k) * size(xs, 3)
+    @views for i in 1:size(xs, 3)
+        history, q = 0, 1
+        for t in 1:dist.k
+            q *= dist.B
+            history = dist.B * history + index(xs[:,t,i], dist.bs) - 1
+        end
+        for t in dist.k+1:size(xs, 2)
+            x = index(xs[:,t,i], dist.bs)
+            dist.future[x] += 1
+            dist.history[history + 1] += 1
+            dist.joint[x, history + 1] += 1
+            history = dist.B * history - q * (index(xs[:,t-dist.k,i], dist.bs) - 1) + x - 1
+        end
+    end
+    dist
+end
+
+function observe!(dist::ActiveInfo, xs::AbstractArray{Int,2})
+    if size(xs, 2) ≤ dist.k
+        throw(ArgumentError("data's length must be greater than the history length"))
+    end
+    dist.N += size(xs, 2) - dist.k
+    history, q = 0, 1
+    @views for t in 1:dist.k
+        q *= dist.B
+        history = dist.B * history + index(xs[:,t], dist.bs) - 1
+    end
+    @views for t in dist.k+1:size(xs, 2)
+        x = index(xs[:,t], dist.bs)
+        dist.future[x] += 1
+        dist.history[history + 1] += 1
+        dist.joint[x, history + 1] += 1
+        history = dist.B * history - q * (index(xs[:,t-dist.k], dist.bs) - 1) + x - 1
+    end
+    dist
+end
+
+function observe!(dist::ActiveInfo, xs::AbstractArray{Int,1})
     if length(xs) ≤ dist.k
         throw(ArgumentError("data's length must be greater than the history length"))
     end
     dist.N += length(xs) - dist.k
     history, q = 0, 1
-    for i in 1:dist.k
-        q *= dist.b
-        history = dist.b * history + xs[i] - 1
+    @views for t in 1:dist.k
+        q *= dist.bs[1]
+        history = dist.bs[1] * history + xs[t] - 1
     end
-    for i in dist.k+1:length(xs)
-        dist.future[xs[i]] += 1
+    @views for t in dist.k+1:length(xs)
+        dist.future[xs[t]] += 1
         dist.history[history + 1] += 1
-        dist.joint[xs[i], history + 1] += 1
-        history = dist.b * history - q * (xs[i - dist.k] - 1) + xs[i] - 1
+        dist.joint[xs[t], history + 1] += 1
+        history = dist.bs[1] * history - q * (xs[t - dist.k] - 1) + xs[t] - 1
     end
     dist
 end
@@ -59,11 +122,11 @@ end
     dist
 end
 
-function activeinfo!(dist::ActiveInfo, xs::AbstractVector{Int})
+function activeinfo!(dist::ActiveInfo, xs::AbstractArray{Int})
     estimate(observe!(dist, xs))
 end
 
-activeinfo(xs::AbstractVector{Int}; kwargs...) = estimate(ActiveInfo(xs; kwargs...))
+activeinfo(xs::AbstractArray{Int}; kwargs...) = estimate(ActiveInfo(xs; kwargs...))
 
 function activeinfo(::Type{Kraskov1}, xs::AbstractMatrix{Float64};
                     k::Int=1, τ::Int=1, nn::Int=1, metric::Metric=Chebyshev())
--- test/ai.jl
+++ test/ai.jl
@@ -1,41 +1,41 @@
 @testset "Active Information" begin
-    @test_throws MethodError ActiveInfo(0.5, 1.0)
+    @test_throws MethodError ActiveInfo(0.5; k=1.0)
     for b in -2:1
-        @test_throws ArgumentError ActiveInfo(b, 2)
+        @test_throws ArgumentError ActiveInfo(b; k=2)
     end
     for k in -2:0
-        @test_throws ArgumentError ActiveInfo(2, k)
+        @test_throws ArgumentError ActiveInfo(2; k)
     end
 
-    let ai = ActiveInfo(2, 1)
+    let ai = ActiveInfo(2; k=1)
         @test ai.joint == zeros(Int, 2, 2)
         @test ai.future == zeros(Int, 2)
         @test ai.history == zeros(Int, 2)
-        @test ai.b == 2
+        @test ai.bs == (2,)
         @test ai.k == 1
         @test ai.N == zero(Int)
     end
 
-    let ai = ActiveInfo(2, 3)
+    let ai = ActiveInfo(2; k=3)
         @test ai.joint == zeros(Int, 2, 8)
         @test ai.future == zeros(Int, 2)
         @test ai.history == zeros(Int, 8)
-        @test ai.b == 2
+        @test ai.bs == (2,)
         @test ai.k == 3
         @test ai.N == zero(Int)
     end
 
-    let ai = ActiveInfo(2, 2)
+    let ai = ActiveInfo(2; k=2)
         observe!(ai, [2,1,2,1])
         @test ai.joint == [0 1 0 0; 0 0 1 0]
         @test ai.future == [1,1]
         @test ai.history == [0,1,1,0]
-        @test ai.b == 2
+        @test ai.bs == (2,)
         @test ai.k == 2
         @test ai.N == 2
     end
 
-    let ai = ActiveInfo(2, 2)
+    let ai = ActiveInfo(2; k=2)
         observe!(ai, [1,1,1,1])
         observe!(ai, [1,2,2,2])
         @test ai.joint == [2 0 0 0; 0 1 0 1]
@@ -44,15 +44,15 @@
         @test ai.N == 4
     end
 
-    @test_throws ArgumentError ActiveInfo([0,1,1]; k=2)
     @test_throws ArgumentError ActiveInfo(Int[]; k=2)
     @test_throws ArgumentError ActiveInfo([1]; k=2)
     @test_throws ArgumentError ActiveInfo([1,2]; k=2)
+    @test_throws BoundsError ActiveInfo([0,1,1]; k=2)
 
-    @test_throws ArgumentError observe!(ActiveInfo(2, 2), [1,2])
-    @test_throws BoundsError observe!(ActiveInfo(2, 2), [1,2,3])
-    @test_throws BoundsError observe!(ActiveInfo(2, 2), [0,0,1,1])
-    @test_throws BoundsError observe!(ActiveInfo(2, 2), [2,3,2,3])
+    @test_throws ArgumentError observe!(ActiveInfo(2; k=2), [1,2])
+    @test_throws BoundsError observe!(ActiveInfo(2; k=2), [1,2,3])
+    @test_throws BoundsError observe!(ActiveInfo(2; k=2), [0,0,1,1])
+    @test_throws BoundsError observe!(ActiveInfo(2; k=2), [2,3,2,3])
 
     let as = [2,2,1,1,2,1,1,2]
         @test activeinfo(as; k=2) ≈ 0.918296 atol=1e-6
@@ -100,6 +100,30 @@
 
     let as = [3,3,3,3,3,3,2,2,2]
         @test activeinfo(as; k=2) ≈ 0.469566 atol=1e-6
+    end
+
+    let as = [2 2 2 1 2 1 1 1 2;
+              2 2 2 2 1 1 1 1 1]
+        @test activeinfo(as; k=2) ≈ 1.270942 atol=1e-6
+    end
+
+    let as = [1 1 2 2 2 2 1 2 1;
+              2 2 2 2 2 2 2 1 1]
+        @test activeinfo(as; k=2) ≈ 1.270942 atol=1e-6
+    end
+
+    let as = [1 1 1 1 1 1 2 2 2;
+              2 2 2 2 2 2 1 1 1]
+        @test activeinfo(as; k=2) ≈ 0.469566 atol=1e-6
+    end
+
+    let as = zeros(Int, 2, 9, 2)
+        as[:,:,1] = [1 1 2 2 2 2 1 2 1;
+                     2 2 2 2 2 2 2 1 1]
+        as[:,:,2] = [1 1 1 1 1 1 2 2 2;
+                     2 2 2 2 2 2 1 1 1]
+
+        @test activeinfo(as; k=2) ≈ 0.959190 atol=1e-6
     end
 
     @testset "AI is a Mutual Information" for k in 1:9
